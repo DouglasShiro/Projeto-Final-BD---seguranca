bd7f22deb9c8d5266d88703c84658f9d286bb00b
DouglasShiro <douglasshiro@hotmail.com>
2015-11-29 12:41:53 -0200

    Projeto NetBeans
    Parte de codificação do projeto final, utilizando a IDE NetBeans para o
    código Java, o banco de dados criado no MySQL, servidor TomCat

---
+++ projeto_final_BD/src/java/modelo/Policial.java
@@ -0,0 +1,20 @@
+/*
+ *             Projeto Final de Banco de Dados 2/2015
+ * Professora: Maristela Terto de Holanda
+ * Componentes:
+ *                 Douglas Shiro Yokoyama      13/0024902
+ *                 Marcelo Andre Winkler                 
+ */
+package modelo;
+
+public class Policial {
+    //Lista de atributos
+    private int id_policial;
+    private String nome;
+    private String titulo;
+    private int telefone;
+    private String endereco;
+            
+    //Métodos get/set
+    
+}
