4eb9714c28ab3a133563c7fe27b10727a84e7f1f
unknown <marsnspace107@hotmail.com>
2015-11-30 11:29:49 -0200

    Terminei ddl_seguranca, troquei as chaves primarias para auto incrementar e adicionei o modelo conecitual. Pode ser que mudar a imagem do modelo conceitual, pois nao lembro se modifiquei algo.

--- ddl_seguranca.sql
+++ ddl_seguranca.sql
@@ -6,8 +6,9 @@ USE segurança;
 
 CREATE TABLE policial
 	(
-		id_policial			integer(10) not null,
-		nome				varchar(20) not null,
+		id_policial			integer unsigned not null auto_increment,
+		num_id				integer(10) not null,
+        nome				varchar(20) not null,
         titulo				varchar(20) not null,
         cep					varchar(15) not null,
         cidade				varchar(30) not null,
@@ -19,28 +20,28 @@ CREATE TABLE policial
 CREATE TABLE telefone_policial
 	(
 		telefone			integer(10) not null,
-        id_policial			integer(10) not null,
+        id_policial			integer unsigned not null,
         PRIMARY KEY (telefone, id_policial),
         FOREIGN KEY (id_policial) REFERENCES policial(id_policial) ON DELETE CASCADE
         );
         
 CREATE TABLE categoria_delegacia
 	(
-		id_categoria		integer(10) not null,
+		id_categoria		integer unsigned not null auto_increment,
         nome				varchar(20) not null,
         PRIMARY KEY (id_categoria)
         );
         
 CREATE TABLE delegacia
 	(
-		id_delegacia		integer(10) not null,
+		id_delegacia		integer unsigned not null auto_increment,
         nome				varchar(20) not null,
         cep					varchar(15) not null,
         cidade				varchar(30) not null,
         bairro				varchar(30) not null,
         estado				varchar(2)  not null,
-        categoria		integer(10) not null,
-        delegado			integer(10) not null,
+        categoria			integer unsigned not null,
+        delegado			integer unsigned not null,
         PRIMARY KEY (id_delegacia),
         FOREIGN KEY (categoria) REFERENCES categoria_delegacia(id_categoria) ON DELETE CASCADE,
         FOREIGN KEY (delegado) REFERENCES policial(id_policial) ON DELETE CASCADE
@@ -49,14 +50,14 @@ CREATE TABLE delegacia
 CREATE TABLE telefone_delegacia
 	(
 		telefone			integer(10) not null,
-        id_delegacia		integer(10) not null,
+        id_delegacia		integer unsigned not null,
         PRIMARY KEY (telefone, id_delegacia),
         FOREIGN KEY (id_delegacia) REFERENCES delegacia(id_delegacia) ON DELETE CASCADE
         );
         
 CREATE TABLE deposito
 	(
-		id_deposito			integer(10) not null,
+		id_deposito			integer unsigned not null auto_increment,
         cep					varchar(15) not null,
         cidade				varchar(30) not null,
         bairro				varchar(30) not null,
@@ -66,35 +67,141 @@ CREATE TABLE deposito
         
 CREATE TABLE categoria_item
 	(
-		id_categoria		integer(10) not null,
+		id_categoria		integer unsigned not null auto_increment,
         nome				varchar(20) not null,
         PRIMARY KEY (id_categoria)
         );
 
 CREATE TABLE item
 	(
-		id_item				integer(10) not null,
+		id_item				integer unsigned not null auto_increment,
 		nome				varchar(20) not null,
-        categoria			integer(10) not null,
+        categoria			integer unsigned not null,
         PRIMARY KEY (id_item),
         FOREIGN KEY (categoria) REFERENCES categoria_item(id_categoria) ON DELETE CASCADE
         );
         
 CREATE TABLE historico_deposito
 	(
-		id_historico		integer(10) not null,
+		id_historico		integer unsigned not null auto_increment,
         status_historico	varchar(30) not null,
-        deposito			integer(10) not null,
-        item				integer(10) not null,
+        deposito			integer unsigned not null,
+        item				integer unsigned not null,
         data_hora			datetime not null,
         PRIMARY KEY (id_historico),
         FOREIGN KEY (item) REFERENCES item(id_item) ON DELETE CASCADE,
         FOREIGN KEY (deposito) REFERENCES deposito(id_deposito) ON DELETE CASCADE
         );
 
-        
-        
-        
+CREATE TABLE cidadao
+	(
+		id_cidadao			integer unsigned not null auto_increment,
+        cpf					integer(11) not null,
+        nome				varchar(20) not null,
+        cep					varchar(15) not null,
+        cidade				varchar(30) not null,
+        bairro				varchar(30) not null,
+        estado				varchar(2)  not null,
+		PRIMARY KEY (id_cidadao)
+        );
 
+CREATE TABLE telefone_cidadao
+	(
+		telefone			integer(10) not null,
+        id_cidadao			integer unsigned not null,
+        PRIMARY KEY (telefone, id_cidadao),
+        FOREIGN KEY (id_cidadao) REFERENCES cidadao(id_cidadao) ON DELETE CASCADE
+        );
+
+CREATE TABLE categoria_objeto_pessoal
+	(
+		id_categoria		integer unsigned not null auto_increment,
+        nome				varchar(20) not null,
+		PRIMARY KEY (id_categoria)
+        );
+
+CREATE TABLE objeto_pessoal
+	(
+		id_obj_pessoal		integer unsigned not null auto_increment,
+        nome				varchar(20) not null,
+        descricao			varchar(100) not null,
+        categoria			integer unsigned not null,
+        PRIMARY KEY (id_obj_pessoal)
+        );
+
+CREATE TABLE categoria_imagem
+	(
+		id_categoria		integer unsigned not null auto_increment,
+        categoria			varchar(30) not null,
+        PRIMARY KEY (id_categoria)
+        );
+
+CREATE TABLE imagem
+	(
+		id_imagem			integer unsigned not null auto_increment,
+        foto				longblob not null,
+        descricao			varchar(100) not null,
+        categoria			integer unsigned not null,
+        PRIMARY KEY (id_imagem),
+        FOREIGN KEY (categoria) REFERENCES categoria_imagem(id_categoria)
+        );
+        
+CREATE TABLE categoria_seguranca
+	(
+		id_categoria		integer unsigned not null auto_increment,
+        nivel				varchar(30) not null,
+        PRIMARY KEY (id_categoria)
+        );
+
+CREATE TABLE bairro
+	(
+		id_bairro			integer unsigned not null auto_increment,
+        nome				varchar(20) not null,
+        categoria_seg		integer unsigned not null,
+        imagem				integer unsigned not null,
+        PRIMARY KEY (id_bairro),
+        FOREIGN KEY (categoria_seg) REFERENCES categoria_seguranca(id_categoria),
+        FOREIGN KEY (imagem) REFERENCES imagem(id_imagem)
+        );
+        
+CREATE TABLE categoria_ocorrencia
+	(
+		id_categoria		integer unsigned not null auto_increment,
+        nome				varchar(30) not null,
+        PRIMARY KEY (id_categoria)
+        );
+
+CREATE TABLE ocorrencia
+	(
+		id_ocorrencia		integer unsigned not null auto_increment,
+        num_ocorrencia		integer(10) not null,
+        observacao			varchar(100) not null,
+        policial			integer unsigned not null,
+        vitima				integer unsigned not null,
+        infrator			integer unsigned,
+        categoria			integer unsigned not null,
+        bairro				integer unsigned not null,
+        obj_pessoal			integer unsigned,
+        PRIMARY KEY (id_ocorrencia),
+        FOREIGN KEY (policial) REFERENCES policial(id_policial),
+        FOREIGN KEY (vitima) REFERENCES cidadao(id_cidadao),
+        FOREIGN KEY (infrator) REFERENCES cidadao(id_cidadao),
+        FOREIGN KEY (categoria) REFERENCES categoria_ocorrencia(id_categoria),
+        FOREIGN KEY (bairro) REFERENCES bairro(id_bairro),
+        FOREIGN KEY (obj_pessoal) REFERENCES objeto_pessoal(id_obj_pessoal)
+        );
+        
+CREATE TABLE historico_ocorrencia
+	(
+		id_historico		integer unsigned not null auto_increment,
+        observacao			varchar(100) not null,
+        data_hora			datetime not null,
+        ocorrencia			integer unsigned not null,
+        PRIMARY KEY (id_historico),
+        FOREIGN KEY (ocorrencia) REFERENCES ocorrencia(id_ocorrencia)
+        );
+        
+		
+		
         
         
